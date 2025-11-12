b73b4b546131623493aa85835c011b2c5dc37933
Enzo Motta <enzomotta70@gmail.com>
2025-11-12 20:27:18 -0300

    first commit

---
+++ src/test/java/br/com/fiap/Clientes/ClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.Clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
