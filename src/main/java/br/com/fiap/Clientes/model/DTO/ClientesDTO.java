941ad9a3f2dac866a196cfb4e8b0d54849ab54ba
Enzo Motta <enzomotta70@gmail.com>
2025-11-13 21:12:04 -0300

    adicionando validation

--- src/main/java/br/com/fiap/Clientes/model/DTO/ClientesDTO.java
+++ src/main/java/br/com/fiap/Clientes/model/DTO/ClientesDTO.java
@@ -1,11 +1,20 @@
 package br.com.fiap.Clientes.model.DTO;
 
 import java.time.LocalDate;
+import jakarta.validation.constraints.*;
 
 public record ClientesDTO(
+        @NotBlank
         String nome,
+        @NotNull
+        @Positive
         int idade,
+        @NotNull
+        @Positive
         Double peso,
+        @NotNull
+        @Positive
         Double altura,
+        @PastOrPresent
         LocalDate inicioDoTratamento
 ) {}
